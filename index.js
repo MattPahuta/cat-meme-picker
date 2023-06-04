5875832013b42596c96344ca5f97131deb07b1a6
Matt Pahuta <mhpahuta@gmail.com>
2023-06-03 19:23:00 -0700

    Add get meme functions and logic for single meme

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios');
+const getImageBtn = document.getElementById('get-image-btn');
+const gifsOnlyOption = document.getElementById('gifs-only-option');
+
+// *** Event listeners *** //
+// *********************** //
+emotionRadios.addEventListener('change', highlightCheckedOption) // listen for change events on emotion radios
+getImageBtn.addEventListener('click', getMatchingCatsArray) // listen for clicks on 'get image' button
+
+function getMatchingCatsArray() {
+  const isGif = gifsOnlyOption.checked; // check if gif option is selected
+
+  if (document.querySelector('input[type="radio"]:checked')) { // handle clicks if no emotion selected
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value;
+    // console.log(selectedEmotion)
+
+    const matchingCats = catsData.filter(cat => {
+      // check if gif option is checked, return appropriate array of cat memes
+      return isGif ? cat.emotionTags.includes(selectedEmotion) && cat.isGif : cat.emotionTags.includes(selectedEmotion);
+    })
+  }
+}
+
+function getSingleCatObject() {
+  const catsArray = getMatchingCatsArray() // get an array of matching cats
+
+  if (catsArray.length === 1) {
+    return catsArray[0];
+  } else {
+    const randomCatIndex = Math.floor(Math.random() * catsArray.length)
+    return catsArray[randomCatIndex]
+  }
+}
+
+function renderCat() {
+  getSingleCatObject()
+}
+
+// add styling to selected emotion radio
+function highlightCheckedOption(e) {
+  // handle highlight style removal - ensure only on item highlighted
+  const radios = document.getElementsByClassName('radio');
+  for (let radio of radios) {
+    radio.classList.remove('highlight');
+  }
+  const selectedEmotion = document.getElementById(e.target.id).parentElement; // get selected radio
+  selectedEmotion.classList.add('highlight'); // add highlight to selected radio
+
+}
 
 // get emotions from the cat arrays
 function getEmotionsArray(cats) {
